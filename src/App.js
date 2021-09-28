cc6ce738483489b428d44383c851b40ee7502722
Joyanta Mondal <56169323+w3codelabs@users.noreply.github.com>
2021-09-28 20:30:11 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className="bg-yellow-200 text-2xl hover:text-danger-200">This is my text</p>
+      <p className="text-custom">Hello </p>
+    </div>
+  );
+}
+
+export default App;
